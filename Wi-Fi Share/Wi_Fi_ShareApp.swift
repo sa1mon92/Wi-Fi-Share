a137dccc8f495ed5d76f7071a35a8ba1d72e7654
Dmitrii Sadyrev <sadyrevdimon@icloud.com>
2025-09-08 11:29:47 +0300

    Initial Commit

---
+++ Wi-Fi Share/Wi_Fi_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wi_Fi_ShareApp.swift
+//  Wi-Fi Share
+//
+//  Created by Садырев Дмитрий on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Wi_Fi_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
